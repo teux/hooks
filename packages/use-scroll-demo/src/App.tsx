65b99cc185d1c2719ffc93e341eabcf827aae513
Andrey Koperskiy <akoperskiy@tsconsulting.com>
2020-07-02 15:59:43 +0300

    Initial commit

---
+++ packages/use-scroll-demo/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useScroll} from '@teux/use-scroll';
+
+function App() {
+  const foo = useScroll();
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ types/webpack-assets/index.d.ts
@@ -0,0 +1,15 @@
+declare module '*.svg' {
+  import { ComponentType, SVGAttributes } from 'react';
+  const content: ComponentType<SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+declare module '*.json' {
+  const content: { [key: string]: any };
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
